d7a18b94de9c0fea75e36f621a9ba24bc17fcb1b
liuzhengfeng <563596927@qq.com>
2025-10-08 17:29:19 +0800

    通过 compression-webpack-plugin 和 nginx 启用gzip压缩

--- build/webpack.prod.ts
+++ build/webpack.prod.ts
@@ -4,6 +4,7 @@ import { merge } from 'webpack-merge'
 import { Configuration } from 'webpack'
 import common from './webpack.common'
 import webpackBundleAnalyzer from 'webpack-bundle-analyzer'
+import CompressionPlugin from 'compression-webpack-plugin'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -15,6 +16,11 @@ const config: Configuration = merge<Configuration>(common, {
       analyzerMode: 'static', // 生成 HTML 文件
       openAnalyzer: false,
       reportFilename: path.resolve(__dirname, 'bundle-report.html')
+    }),
+    new CompressionPlugin({
+      test: /\.(js|css|html|svg)$/,
+      algorithm: 'gzip',
+      threshold: 10240
     })
   ]
 })
